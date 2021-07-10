4eaf245e2f09e7f6c79da5e72a45d903a0a33fa4
ogumusay <gumusayogulcan@gmail.com>
2021-07-10 18:17:14 +0300

    Created game over system

---
+++ Assets/Scripts/Game/GameOverManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hexagon.Board;
+using Hexagon.GameObjects;
+using Hexagon.State;
+
+
+namespace Hexagon.Game
+{
+    public class GameOverManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gameOverUI;
+
+        private void Awake()
+        {
+            BoardManager.OnNoMoveLeft += EndGame;
+            HexagonBombObject.OnBombExplodes += EndGame;
+        }
+
+        private void EndGame()
+        {
+            Debug.Log("game over");
+
+            StateManager.CurrentState = StateManager.State.GAME_OVER;
+
+            _gameOverUI.SetActive(true);
+        }
+
+        private void OnDisable()
+        {
+            BoardManager.OnNoMoveLeft -= EndGame;
+            HexagonBombObject.OnBombExplodes -= EndGame;
+        }
+    }
+}
